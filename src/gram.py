b4195bde45db9e7f89b3bee18c091bad21df61c8
Smazle <August.vinkel@gmail.com>
2017-11-26 16:53:05 +0100

    Added word counter, and fixed doc stringjk:w

--- src/gram.py
+++ src/gram.py
@@ -1,11 +1,13 @@
 from collections import Counter
 
 def find_ngrams(filepath, n):
-    # Gets the character n-grams of provided texts, and
-    # returns counts
-    # Arg, filepath: Path to file, to be read. String
-    # Arg, n: Which kind of gram to compute
-    # Returns counting dictionary, of the n-grams contained in file
+    """
+        Gets the character n-grams of provided texts, and
+        returns counts
+        Arg, filepath: Path to file, to be read. String
+        Arg, n: Which kind of gram to compute
+        Returns counting dictionary, of the n-grams contained in file
+    """
 
     fileStr = open(filepath, "r").read()
     a = [fileStr[i:i+n][:] for i in range(len(fileStr)-n)]
---
+++ src/words.py
@@ -0,0 +1,16 @@
+from collections import Counter
+
+def words(filepath):
+    """
+        Gets word count dictionary, of file. Removing everything
+        but chars also in the string.ascii_letters list
+        Arg, filepath: String describing filepath, of file to be counted
+        Returns: Counting dictionary over all words in the text file provided
+    """
+    fileStr = open(filepath, "r").read()
+    fileStr = ''.join([char if char in string.ascii_letters \
+                or char == " " else " " for char in fileStr])
+    
+    ret =  Counter(fileStr.split(' '))
+    del ret['']
+    return ret
