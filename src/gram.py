569b4ef17f8953281fccc5b131f53218b7098ca2
Magnus Stavngaard <magnus@stavngaard.dk>
2017-12-05 10:14:49 +0100

    Add function to find n-gram frequency

--- src/gram.py
+++ src/gram.py
@@ -1,16 +1,52 @@
 #!/usr/bin/python3
+
 from collections import Counter
+import sys
 
 
-def find_ngrams(filepath, n):
+def n_gram_number(string_length, n):
+    """
+        Compute number of n-grams in a string of length string_length.
+        Arg, string_length: Length of string.
+        Arg, n: N-gram length.
+        Returns number of n-grams.
+    """
+    return string_length - n
+
+
+def find_ngrams(f, n):
     """
         Gets the character n-grams of provided texts, and
         returns counts
-        Arg, filepath: Path to file, to be read. String
-        Arg, n: Which kind of gram to compute
-        Returns counting dictionary, of the n-grams contained in file
+        Arg, f: File object.
+        Arg, n: Which kind of gram to compute.
+        Returns counting dictionary, of the n-grams contained in file.
     """
 
-    fileStr = open(filepath, 'r').read()
-    a = [fileStr[i:i + n][:] for i in range(len(fileStr) - n)]
-    return Counter(a)
+    content = f.read() if type(f) is file else f
+    n_grams = [content[i:i + n][:] for i in range(len(content) - n)]
+
+    return Counter(n_grams)
+
+
+def find_frequencies(f, n_grams, n):
+    """
+        Get frequencies of specific n-grams in the order they appear in the
+        list.
+        Arg, f: File object.
+        Arg, n_grams: List of n-grams to extract frequencies for.
+        Arg, n: Lenght of n-grams.
+        Returns The frequencies of each of the specific n-grams given.
+    """
+
+    content = f.read() if type(f) is file else f
+    gram_n = n_gram_number(len(content), n)
+
+    frequencies = [c / float(gram_n) for c in
+                   (content.count(n_gram) for n_gram in n_grams)]
+
+    return frequencies
+
+
+if __name__ == '__main__':
+    print(find_ngrams(sys.stdin, 3))
