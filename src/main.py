ff538386dc5e22398320e26ef848ae814d5de26d
Magnus Stavngaard <magnus@stavngaard.dk>
2017-12-05 09:09:01 +0100

    Print most frequent grams

--- src/main.py
+++ src/main.py
@@ -3,10 +3,15 @@
 from gram import find_ngrams
 import glob
 
-
+# Location of training data.
 DATA_FOLDER = './data/'
+
+# Path to a file that will be created containing a concatenation of all files
+# in the training data set.
 FULL_TEXT_FILE = './full_text.txt'
 
+# Number of features to use for each n-gram.
+GRAM_FEATURE_NUMBER = 500
 
 # Construct text consisting of a concatenation of all texts in training data.
 files = glob.glob(DATA_FOLDER + '/*/*.txt')
@@ -17,4 +22,4 @@ with open(FULL_TEXT_FILE, 'w') as outfile:
 
 # Find most frequent n-grams over all texts.
 grams = find_ngrams(FULL_TEXT_FILE, 3)
-print(grams)
+print(grams.most_common(GRAM_FEATURE_NUMBER))
