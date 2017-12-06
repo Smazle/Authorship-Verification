5e8800530ba2252b3d782b4f351e985d74af2435
Magnus Stavngaard <magnus@stavngaard.dk>
2017-12-06 18:01:13 +0100

    Fix naming

--- src/words.py
+++ src/words.py
@@ -9,14 +9,14 @@ class CountFeatureExtractor:
         self.size = size
 
     def fit(self, text):
-        words = Count(text)
+        words = find_word_count(text)
         most_common = words.most_common(self.size)
         most_common = [key for (key, value) in most_common]
 
         self.words = most_common
 
     def extract(self, text):
-        return Frequency(text, self.words)
+        return find_word_frequencies(text, self.words)
 
 
 class NGramsFeatureExtractor:
@@ -26,15 +26,14 @@ class NGramsFeatureExtractor:
         self.size = size
 
     def fit(self, text):
-        grams = NGramCount(text, self.n)
+        grams = find_word_n_grams(text, self.n)
         most_common = grams.most_common(self.size)
         most_common = [key for (key, value) in most_common]
 
         self.grams = most_common
-        print(most_common)
 
     def extract(self, text):
-        return NGramFrequency(text, self.n, self.grams)
+        return find_word_n_gram_frequencies(text, self.n, self.grams)
 
 
 def find_words(text):
@@ -53,7 +52,7 @@ def find_words(text):
     return nltk.word_tokenize(text)
 
 
-def Count(text):
+def find_word_count(text):
     """
         Makes use of the find_words function to get the list of
         words contained within the text, and then returns the
@@ -68,7 +67,7 @@ def Count(text):
     return Counter(find_words(text))
 
 
-def Frequency(text, wordsList):
+def find_word_frequencies(text, wordsList):
     """
         Computes the frequencies of words present
         in a supplied text.
@@ -83,13 +82,13 @@ def Frequency(text, wordsList):
                 in wordsList, in the same order as wordsList
 
     """
-    wordCounts = Count(text)
+    wordCounts = find_word_count(text)
     total = float(len(wordCounts))
     return [wordCounts[key] / total if key in wordCounts else 0
             for key in wordsList]
 
 
-def NGramCount(text, n):
+def find_word_n_grams(text, n):
     """
         Computes the count of the word n-grams
         that appear in the provided text.
@@ -106,23 +105,23 @@ def NGramCount(text, n):
     return Counter(grams)
 
 
-def NGramFrequency(text, n, ngramList):
+def find_word_n_gram_frequencies(text, n, n_grams):
     """
-        Computes the Frequency of a specified list
+        Computes the find_word_frequencies of a specified list
         of n-grams in the provided text string.
 
         Args:
             text (string): Text to be analysed for word n-gram counts
             n (int): Which kind of gram to be used
-            ngramList (list): A list of strings describing the word n-grams
+            n_grams (list): A list of strings describing the word n-grams
                 where a frequency is desired
         Returns:
             list[int]: A list containing the frequencies of supplied words
-                in the order of ngramList
+                in the order of n_grams
     """
 
-    ngramCounts = NGramCount(text, n)
-    total = float(len(ngramCounts))
+    n_gram_counts = find_word_n_grams(text, n)
+    total = float(len(n_gram_counts))
 
-    return [ngramCounts[key] / total if key in ngramCounts
-            else 0 for key in ngramList]
+    return [n_gram_counts[key] / total if key in n_gram_counts
+            else 0 for key in n_grams]
