57457575d6c62d6cab95001b19ecabaeef94f456
Magnus Stavngaard <magnus@stavngaard.dk>
2017-12-14 22:43:31 +0100

    Testing

--- test/delta/Delta.hs
+++ test/delta/Delta.hs
@@ -6,15 +6,28 @@ import qualified System.IO as S
 import qualified System.IO.Temp as S
 import qualified System.Process as S
 
-main :: IO ()
-main = S.withSystemTempFile "" $ \path h -> do
-    createFeatures path
-    res <- C.replicateM 100 $ runTest path
-    print res
-    print $ sum res / fromIntegral (length res)
+type OpposingSize = Int
+type DataFolder = FilePath
+type WordFrequencies = Int
+data Config = Config DataFolder WordFrequencies OpposingSize
+  deriving (Show)
 
-createFeatures :: FilePath -> IO ()
-createFeatures file = do
+main :: IO ()
+main = do
+    C.mapM testDelta configs
+
+testDelta :: Config -> IO ()
+testDelta (Config dat wordFreq oppositionSize) =
+    S.withSystemTempFile "" $ \path h -> do
+        print (dat, wordFreq, oppositionSize)
+
+        createFeatures dat wordFreq path
+        res <- C.replicateM 10 $ runTest oppositionSize path
+
+        print $ sum res / fromIntegral (length res)
+
+createFeatures :: FilePath -> Int -> FilePath -> IO ()
+createFeatures infile wordFreqs outfile = do
     (_, _, _, processHandle) <- S.createProcess (S.proc program args)
     exitCode <- S.waitForProcess processHandle
 
@@ -22,29 +35,33 @@ createFeatures file = do
   where
     program = "../../feature_extraction/main.py"
     args =
-        [ "../../data/pan_2015/"
-        , file
-        , "--word-frequencies", "150"
+        [ infile
+        , outfile
+        , "--word-frequencies", show wordFreqs
         ]
 
-runTest :: FilePath -> IO Double
-runTest file = do
+runTest :: Int -> FilePath -> IO Double
+runTest opposingSize file = do
     (_, Just hout, _, ph) <- S.createProcess (S.proc program args)
         { S.std_out = S.CreatePipe }
 
     exitCode <- S.waitForProcess ph
     C.unless (isSuccess exitCode) $ S.die "Delta failed"
 
-    read . init <$> S.hGetContents hout
+    read <$> S.hGetLine hout
   where
     program = "../../machine_learning/delta.py"
     args =
         [ file
         , "--with-normalization"
-        , "--opposing-set-size"
-        , "1"
+        , "--opposing-set-size", show opposingSize
         ]
 
 isSuccess :: S.ExitCode -> Bool
 isSuccess S.ExitSuccess = True
 isSuccess _ = False
+
+configs :: [Config]
+configs = Config <$> ["../../data/pan_2013", "../../data/pan_2015"]
+    <*> [100, 150..300]
+    <*> [1..10]
