28c0fe4b80fc5988e5d2ce668876afa2ad0b05d7
Magnus Stavngaard <magnus@stavngaard.dk>
2017-12-14 13:26:33 +0100

    Cleanup whitespace

--- machine_learning/forest.py
+++ machine_learning/forest.py
@@ -1,38 +1,40 @@
 #!/usr/bin/python3
 
-import sys
-import numpy as np
 import argparse
+import numpy as np
 from sklearn.ensemble import RandomForestClassifier
 
 
 # Set up arguments
-parser = argparse.ArgumentParser(description=
-        "Run Random Forest on extracted features")
+parser = argparse.ArgumentParser(
+    description="Run Random Forest on extracted features")
 
 parser.add_argument('file', type=str, help='Data File Location', nargs="+")
-parser.add_argument('--split', type=float, nargs='?', help='The percentage of the data used for training')
+parser.add_argument(
+    '--split', type=float, nargs='?',
+    help='The percentage of the data used for training')
 
 args = parser.parse_args()
 datafiles = args.file
 
 if args.split is None:
-    args.split = .5;
+    args.split = .5
+
 
 def encode(allText, known, unknown):
-    return (np.power(known - unknown, 2) + 1) / (np.power(allText - unknown, 2) + 1)
-
+    return (np.power(known - unknown, 2) + 1) /\
+        (np.power(allText - unknown, 2) + 1)
 
 
 # Remove author number
 X = np.loadtxt(datafiles[0], dtype=np.float)[:, :-1]
-y = X[:,-1]
+y = X[:, -1]
 
 feature_n = X.shape[1] - 1
 feature_n_half = int(feature_n / 2)
 
-X_known = X[:,0:feature_n_half]
-X_unknown = X[:,feature_n_half:-1]
+X_known = X[:, 0:feature_n_half]
+X_unknown = X[:, feature_n_half:-1]
 
 X = np.column_stack((X_known, X_unknown))
 
@@ -41,13 +43,13 @@ X = np.column_stack((X_known, X_unknown))
 # then encode it, as per pancho.
 if len(datafiles) > 1:
     allText = np.loadtxt(datafiles[1], dtype=np.float)
-    
+
     newX = []
-    for known, unknown in zip(X_known, X_unknown): 
+    for known, unknown in zip(X_known, X_unknown):
         newX.append(encode(allText, known, unknown))
 
-    X = np.array(newX)    
-    
+    X = np.array(newX)
+
 
 # Get training and test set split - Randomly
 np.random.shuffle(X)
@@ -65,4 +67,3 @@ model.fit(XTrain, yTrain)
 
 predictions = model.predict(XTest) == yTest
 print("Correct Random Forest", np.sum(predictions))
-
