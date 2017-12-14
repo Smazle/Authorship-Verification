4c38e32b817b8618c2f6ffbf8852ffc1d7cb79cf
Smazle <August.vinkel@gmail.com>
2017-12-14 14:32:34 +0100

    Added condition to the split

--- machine_learning/forest.py
+++ machine_learning/forest.py
@@ -28,7 +28,7 @@ def encode(allText, known, unknown):
 
 # Remove author number
 X = np.loadtxt(datafiles[0], dtype=np.float)[:, :-1]
-y = X[:, -1]
+y = [int(i) for i in X[:, -1]]
 
 feature_n = X.shape[1] - 1
 feature_n_half = int(feature_n / 2)
@@ -52,18 +52,25 @@ if len(datafiles) > 1:
 
 
 # Get training and test set split - Randomly
-np.random.shuffle(X)
 boundary = int(np.floor(len(X) * args.split))
-
+np.random.shuffle(X)
+    
 XTrain = X[:boundary]
 yTrain = y[:boundary]
 
-XTest = X[boundary:]
-yTest = y[boundary:]
+if(args.split == 1.0):
+    XTest = XTrain
+    yTest = yTrain
+else:
+    XTest = X[boundary:]
+    yTest = y[boundary:]
 
 # Create model
-model = RandomForestClassifier()
+model = RandomForestClassifier(n_estimators = 10)
 model.fit(XTrain, yTrain)
 
 predictions = model.predict(XTest) == yTest
-print("Correct Random Forest", np.sum(predictions))
+print("Correct Random Forest", np.sum(predictions)/float(len(predictions)))
+
+
+
