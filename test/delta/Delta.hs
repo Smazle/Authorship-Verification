69841b51eb2bc188e4c54d397f9a838a47be5d73
Magnus Stavngaard <magnus@stavngaard.dk>
2017-12-20 10:55:29 +0100

    Fix test
     * Prevent generation of the same features multiple times.
    
     * Run 100 tests of each configuration.
    
     * Use [100,200..500] word frequencies instead of [100,150..300].

--- test/delta/Delta.hs
+++ test/delta/Delta.hs
@@ -1,33 +1,52 @@
 module Main (main) where
 
 import qualified Control.Monad as C
+import qualified Data.Map as Map
 import qualified System.Exit as S
 import qualified System.IO as S
 import qualified System.IO.Temp as S
 import qualified System.Process as S
 
+type Accuracy = Double
+
 type OpposingSize = Int
 type DataFolder = FilePath
 type WordFrequencies = Int
-data Config = Config DataFolder WordFrequencies OpposingSize
+
+data Corpus = Brown | All
+  deriving (Show, Eq, Ord)
+
+data FeatureConfig = FeatureConfig DataFolder WordFrequencies Corpus
+  deriving (Show, Eq, Ord)
+
+newtype DeltaConfig = DeltaConfig OpposingSize
+  deriving (Show)
+
+data Config = Config FeatureConfig DeltaConfig
   deriving (Show)
 
 main :: IO ()
 main = do
-    C.mapM testDelta configs
+    results <- C.mapM (uncurry testFeatures) myConfigs
+    print $ zip results myConfigs
+  where
+    myConfigs = Map.toList . collapseFeatures $ configs
 
-testDelta :: Config -> IO ()
-testDelta (Config dat wordFreq oppositionSize) =
-    S.withSystemTempFile "" $ \path h -> do
-        print (dat, wordFreq, oppositionSize)
+testFeatures :: FeatureConfig -> [DeltaConfig] -> IO [Accuracy]
+testFeatures featureConfig deltaConfigs = S.withSystemTempFile "" $ \fp h -> do
+    print ("Handling", featureConfig)
+    createFeatures fp featureConfig
+    mapM (testDelta fp) deltaConfigs
 
-        createFeatures dat wordFreq path
-        res <- C.replicateM 10 $ runTest oppositionSize path
+testDelta :: FilePath -> DeltaConfig -> IO Accuracy
+testDelta infile (DeltaConfig oppositionSize) = do
+    print ("Handling", DeltaConfig oppositionSize)
+    results <- C.replicateM 100 $ runTest oppositionSize infile
 
-        print $ sum res / fromIntegral (length res)
+    return $ sum results / fromIntegral (length results)
 
-createFeatures :: FilePath -> Int -> FilePath -> IO ()
-createFeatures infile wordFreqs outfile = do
+createFeatures :: FilePath -> FeatureConfig -> IO ()
+createFeatures outfile (FeatureConfig infile wordFreqs corpus) = do
     (_, _, _, processHandle) <- S.createProcess (S.proc program args)
     exitCode <- S.waitForProcess processHandle
 
@@ -38,6 +57,7 @@ createFeatures infile wordFreqs outfile = do
         [ infile
         , outfile
         , "--word-frequencies", show wordFreqs
+        , "--corpus", corpusName corpus
         ]
 
 runTest :: Int -> FilePath -> IO Double
@@ -57,11 +77,24 @@ runTest opposingSize file = do
         , "--opposing-set-size", show opposingSize
         ]
 
+corpusName :: Corpus -> String
+corpusName Brown = "brown"
+corpusName All = "all"
+
 isSuccess :: S.ExitCode -> Bool
 isSuccess S.ExitSuccess = True
 isSuccess _ = False
 
 configs :: [Config]
-configs = Config <$> ["../../data/pan_2013", "../../data/pan_2015"]
-    <*> [100, 150..300]
-    <*> [1..10]
+configs = Config <$> featureConfigs <*> deltaConfigs
+  where
+    featureConfigs = FeatureConfig <$>
+        ["../../data/pan_2013", "../../data/pan_2015"] <*>
+        [100, 200..500] <*>
+        [Brown]
+    deltaConfigs = DeltaConfig <$> [1..10]
+
+collapseFeatures :: [Config] -> Map.Map FeatureConfig [DeltaConfig]
+collapseFeatures = foldr collapse Map.empty
+  where
+    collapse (Config fc dc) = Map.insertWith (++) fc [dc]
