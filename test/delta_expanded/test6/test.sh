926a18ca7788ac51a77e1236ad9bf619d902c526
Magnus Stavngaard <magnus@stavngaard.dk>
2017-12-28 23:34:20 +0100

    Add a whole bunch of tests

---
+++ test/delta_expanded/test6/test.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+../../feature_extraction/main.py ../../data/pan_2013/ ./13.txt \
+    --char-n-gram 2 3 4 5 6 --char-n-gram-size 20 \
+    --normalize false --corpus brown
+
+../../feature_extraction/main.py ../../data/pan_2015/ ./15.txt \
+    --char-n-gram 2 3 4 5 6 --char-n-gram-size 20 \
+    --normalize false --corpus brown
+
+mkdir 13
+mkdir 15
+
+for i in {1..10};
+do
+    echo $i
+    for j in {1..100};
+    do
+        ../../machine_learning/delta.py 13.txt --with-normalization --opposing-set-size $i
+    done | ../Mean > ./13/$i.txt
+
+    for j in {1..100};
+    do
+        ../../machine_learning/delta.py 15.txt --with-normalization --opposing-set-size $i
+    done | ../Mean > ./15/$i.txt
+done
