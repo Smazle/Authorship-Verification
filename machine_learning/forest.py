1d4dcd0593968ed598fb6801e9856b25a97b79d8
Smazle <August.vinkel@gmail.com>
2018-01-04 15:08:45 +0100

    Added test script for RF

--- machine_learning/forest.py
+++ machine_learning/forest.py
@@ -1,4 +1,5 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+
 import argparse
 import numpy as np
 import pickle
---
+++ test/forest/panchofy.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+
+import numpy as np
+import pickle
+import sys
+
+
+datafiles = sys.argv[1:]
+
+model = pickle.load(open('forest.pickle', 'rb'))
+
+
+def encode(allText, known, unknown):
+    return (np.power(known - unknown, 2) + 1) /\
+        (np.power(allText - unknown, 2) + 1)
+
+
+X = np.loadtxt(datafiles[0], dtype=np.float)[:, :-1]
+y = X[:, -1].astype(np.int)
+
+feature_n = X.shape[1] - 1
+feature_n_half = int(feature_n / 2)
+X_known = X[:, 0:feature_n_half]
+X_unknown = X[:, feature_n_half:-1]
+
+master = np.loadtxt(datafiles[1], dtype=np.float)
+
+newX = []
+
+for known, unknown in zip(X_known, X_unknown):
+    newX.append(encode(master, known, unknown))
+
+X = np.array(newX)
+
+res = model.predict(X) == y
+print('Precision: ', np.sum(res) / float(len(res)))
