e6f300b245df749873918c51a2782097f31ed58b
Magnus Stavngaard <magnus@stavngaard.dk>
2018-01-06 20:07:14 +0100

    Add more results from Delta method

--- test/delta/delta_test.sh
+++ test/delta/delta_test.sh
@@ -1,44 +1,63 @@
 #!/usr/bin/env bash
 
+if [ ! -f ./13_features_train.txt ]; then
+    echo "Extracting features for PAN2013 TRAIN"
+    ../../feature_extraction/main.py ../../data/pan_2013/ ./13_features_train.txt \
+        --corpus brown \
+        --normalize false \
+        --word-frequencies 300
+fi
+
+if [ ! -f ./15_features_train.txt ]; then
+    echo "Extracting features for PAN2015 TRAIN"
+    ../../feature_extraction/main.py ../../data/pan_2015/ ./15_features_train.txt \
+        --corpus brown \
+        --normalize false \
+        --word-frequencies 300
+fi
+
 if [ ! -f ./13_features_1.txt ]; then
-    echo "Extracting test features for PAN2013 1"
+    echo "Extracting features for PAN2013 TEST 1"
     ../../feature_extraction/main.py ../../data/pan_2013_test_1/ ./13_features_1.txt \
         --corpus brown \
+        --normalize false \
         --word-frequencies 300
 fi
 
 if [ ! -f ./13_features_2.txt ]; then
-    echo "Extracting test features for PAN2013 2"
+    echo "Extracting features for PAN2013 TEST 2"
     ../../feature_extraction/main.py ../../data/pan_2013_test_2/ ./13_features_2.txt \
         --corpus brown \
+        --normalize false \
         --word-frequencies 300
 fi
 
 if [ ! -f ./15_features.txt ]; then
-    echo "Extracting test features for PAN2015"
+    echo "Extracting features for PAN2015 TEST"
     ../../feature_extraction/main.py ../../data/pan_2015/ ./15_features.txt \
         --corpus brown \
+        --normalize false \
         --word-frequencies 300
 fi
 
-#echo "Using 13 1 features"
-#for i in {0..20}
-#do
-    #for j in {1..100}
-    #do
-        #../../machine_learning/delta.py ./13_features_1.txt \
-            #--with-normalization \
-            #--opposing-set-size $i \
-            #--with-PN
-    #done | ./analyse.hs
-#done
+echo "Using 13 1 features"
+for i in {0..20}
+do
+    for j in {1..100}
+    do
+        ./delta.py ./13_features_train.txt ./13_features_1.txt \
+            --with-normalization \
+            --opposing-set-size $i \
+            --with-PN
+    done | ./analyse.hs
+done
 
 echo "Using 13 2 features"
 for i in {0..20}
 do
     for j in {1..100}
     do
-        ../../machine_learning/delta.py ./13_features_2.txt \
+        ./delta.py ./13_features_train.txt ./13_features_2.txt \
             --with-normalization \
             --opposing-set-size $i \
             --with-PN
@@ -50,7 +69,7 @@ for i in {0..20}
 do
     for j in {1..100}
     do
-        ../../machine_learning/delta.py ./15_features.txt \
+        ./delta.py ./15_features_train.txt ./15_features.txt \
             --with-normalization \
             --opposing-set-size $i \
             --with-PN
