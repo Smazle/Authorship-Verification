98f344dda9b040f091ef9199c2720a32f82363c3
Smazle <August.vinkel@gmail.com>
2018-01-15 18:04:59 +0100

    added feature importances graphs

---
+++ report/graph_generation/forest_importance.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+data = np.loadtxt("../data/BrownMinusImportance2015.output", dtype=str, delimiter="\n")
+data = [x.strip("'()").split(", ") for x in data]
+data = [("".join(x[:-1]), x[-1]) for x in data]
+
+idx = 0
+
+if idx != 0:
+    data = data[:idx]
+
+    plt.bar(range(len(data)), [float(x[1]) for x in data])
+    plt.xticks(range(len(data)), [x[0].replace("b\\","").replace("\\","") for x in data], rotation=30, horizontalalignment="right")
+    
+    plt.title(str(idx) + "most important features for UBM Random Forest")
+    plt.xlabel("Text Feature")
+    plt.ylabel("Importance of feature")
+    plt.show()
+else:
+    plt.bar(range(len(data)), [float(x[1]) for x in data])
+    plt.title("Feature importance for UBM Random Forest")
+    plt.xlabel("Text Feature Index")
+    plt.ylabel("Importance of feature")
+    plt.show()
